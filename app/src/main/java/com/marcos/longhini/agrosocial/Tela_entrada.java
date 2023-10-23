86c847b26168c2c5a6b0e29054f9cb2bcc0b95d1
User <longhinimarcos@gmail.com>
2023-10-23 17:31:36 -0300

    Alterei a Tela: Tela_entrada

--- app/src/main/java/com/marcos/longhini/agrosocial/Tela_entrada.java
+++ app/src/main/java/com/marcos/longhini/agrosocial/Tela_entrada.java
@@ -2,7 +2,9 @@ package com.marcos.longhini.agrosocial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Tela_entrada extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class Tela_entrada extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_entrada);
+
     }
+
+    public void Login(View view) {
+        Intent intent = new Intent(Tela_entrada.this,Login.class);
+        startActivity(intent);
+
+    }
+
+    public void Cadastro(View view) {
+        Intent intent = new Intent(Tela_entrada.this,CadastroActivity.class);
+        startActivity(intent);
+    }
+
+
 }
